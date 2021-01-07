69c10682ca410a44386585dfab7481455f7303a2
Dmytro <jerod.a.edein@gmail.com>
2021-01-07 19:14:11 +0200

    initial commit

---
+++ src/main/java/com/vasylenko/demotutorialapp/DemoTutorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.vasylenko.demotutorialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTutorialAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTutorialAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vasylenko/demotutorialapp/DemoTutorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasylenko.demotutorialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTutorialAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
